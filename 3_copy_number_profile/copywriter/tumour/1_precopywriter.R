18c8468d51bca05b3540fe3ce81ab1af1b0ed4c6
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-22 15:37:53 +0100

    Create 1_precopywriter.R

---
+++ 3_copy_number_profile/copywriter/tumour/1_precopywriter.R
@@ -0,0 +1,7 @@
+library('CopyhelpeR')
+library('CopywriteR')
+
+preCopywriteR(output.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/copywriter/precopywriter"),
+                bin.size = 20000,
+                ref.genome = "mm10",
+                prefix = "")
