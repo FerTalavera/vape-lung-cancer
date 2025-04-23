46ae79790cf4bd4dd9b428d77f88eef4524e62f6
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-23 10:19:26 +0100

    Update precopywriter.R

--- 3_copy_number_profile/copywriter/tumour/precopywriter.R
+++ 3_copy_number_profile/copywriter/tumour/precopywriter.R
@@ -1,7 +1,7 @@
 library('CopyhelpeR')
 library('CopywriteR')
 
-preCopywriteR(output.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/"),
+preCopywriteR(output.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/precopywriter"),
                 bin.size = 20000,
                 ref.genome = "mm10",
                 prefix = "")
