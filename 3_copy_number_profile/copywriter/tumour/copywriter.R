8f668a2fe5e5a9e53d01238ee7f67adcdcdfd0fa
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-23 10:45:28 +0100

    Update copywriter.R

--- 3_copy_number_profile/copywriter/tumour/copywriter.R
+++ 3_copy_number_profile/copywriter/tumour/copywriter.R
@@ -1,30 +1,9 @@
 library("CopywriteR")
-
-bp.param <- SnowParam(workers = 1, type = "SOCK")
-
-name = "MD6753"
-
-tumor_bam = paste("/mnt/Adenina/drobles/data/smoking_mice/PDexport/2499_",name,"a/mapped_sample/MOUSE_GRCm38_pulldown_",name,"a.dupmarked.bam", sep="") 
-normal_bam = paste("/mnt/Adenina/drobles/data/smoking_mice/PDexport/2499_",name,"c/mapped_sample/MOUSE_GRCm38_pulldown_",name,"c.dupmarked.bam", sep="") 
-
-sample.control = data.frame(samples=c(normal_bam,tumor_bam),controls=c(normal_bam,normal_bam))
-
-CopywriteR(sample.control = sample.control,
-             destination.folder = file.path(paste("/mnt/atgc-d1/drobles/ftalavera/copywriter/results/",name, sep="")),
-             reference.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/copywriter/precopywriter", "mm10_20kb"),
-             capture.regions.file = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/reference/agilent_bait/Allexon_v2_Covered_corrected.bed"), bp.param = bp.param)
-
-plotCNA(destination.folder = file.path(paste("/mnt/atgc-d1/drobles/ftalavera/copywriter/results/",name, sep="")))
-
-
-
-library("CopywriteR")
 library("BiocParallel")
 
 bp.param <- SnowParam(workers = 1, type = "SOCK")
 
-# Replace with your actual sample names
-names <- c("MD6753", "MD6754", "MD6755")
+names <- c("MD6753", "MD6754", "MD6755", "MD6756", "MD6758")
 
 for (name in names) {
   tumor_bam <- paste0("/mnt/Adenina/drobles/data/smoking_mice/PDexport/2499_", name, "a/mapped_sample/MOUSE_GRCm38_pulldown_", name, "a.dupmarked.bam")
