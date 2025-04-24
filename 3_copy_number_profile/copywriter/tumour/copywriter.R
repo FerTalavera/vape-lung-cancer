ace59769f5083ef54ba01a7caaab9c193df65e69
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-24 10:26:41 +0100

    Update copywriter.R

--- 3_copy_number_profile/copywriter/tumour/copywriter.R
+++ 3_copy_number_profile/copywriter/tumour/copywriter.R
@@ -23,3 +23,21 @@ for (name in names) {
 
   plotCNA(destination.folder = dest_folder)
 }
+
+library("CopywriteR")
+
+bp.param <- SnowParam(workers = 1, type = "SOCK")
+
+name = "MD6753"
+
+tumor_bam <- paste0("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/data/",name, "a_clip.bam", sep="")
+normal_bam = paste("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/data/",MOUSE_GRCm38_pulldown_",name,"c.dupmarked.bam", sep="") 
+
+sample.control = data.frame(samples=c(normal_bam,tumor_bam),controls=c(normal_bam,normal_bam))
+
+CopywriteR(sample.control = sample.control,
+             destination.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/results"),
+             reference.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/precopywriter", "mm10_20kb"),
+             capture.regions.file = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/reference/agilent_bait/Allexon_v2_Covered_corrected.bed"), bp.param = bp.param)
+
+plotCNA(destination.folder = file.path("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/results"))
