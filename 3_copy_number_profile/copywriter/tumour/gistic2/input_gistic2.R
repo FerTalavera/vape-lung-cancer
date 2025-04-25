0a37497ff5a2c3c5993aee9ece4966ea28e2437e
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-25 16:31:09 +0100

    Update input_gistic2.R

--- 3_copy_number_profile/copywriter/tumour/gistic2/input_gistic2.R
+++ 3_copy_number_profile/copywriter/tumour/gistic2/input_gistic2.R
@@ -1,12 +1,15 @@
-setwd(paste("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/MD6753/CNAprofiles/",sep=""))
+library(DNAcopy)
 
-load("segment.Rdata")
-segmentData = segment.CNA.object$output
+load("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/MD6753/CNAprofiles/segment.Rdata")
+segmentation.values <- segment.CNA.object$output
+colnames(segmentation.values) <- c("Sample", "Chromosome", "Start Position", "End Position",
+                                   "Num markers", "Seg.CN")
+write.table(segmentation.values, file = "/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/gistic2/input/MD6753_segmentation_values.tsv", quote = FALSE,
+            row.names = FALSE, sep = "\t")
 
-Selection = unique(grep("log2.MD6753a_clip.bam.vs.log2.MOUSE_GRCm38_pulldown_MD6753c.dupmarked.bam",segmentData$ID,value=T))
-
-segmentData = segmentData[segmentData$ID==Selection,]
-
-segmentData = segmentData[,c("chrom","loc.start","loc.end", "num.mark","seg.mean")]
-colnames(segmentData)=c("Chrom","Start","End","Markers","Mean")
-write.table(segmentData,file=paste("/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/gistic2/input/MD6753a.Copywriter.segments.txt",sep=""),quote=F,sep="\t",row.names=F,col.names=T)
+markers <- data.frame(paste(segment.CNA.object$data$chrom, segment.CNA.object$data$maploc,
+                            sep = ":"),
+                      segment.CNA.object$data$chrom, segment.CNA.object$data$maploc)
+colnames(markers) <- c("Marker Name", "Chromosome", "Marker Position")
+write.table(markers, file = "/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/3_copy_number_profile/copywriter/tumour/gistic2/input/markers.tsv", quote = FALSE, row.names = FALSE,
+            sep = "\t")
