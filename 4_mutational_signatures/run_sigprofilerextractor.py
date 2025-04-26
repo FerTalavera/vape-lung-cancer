d42a8d5678811d53f59409a249d92cad6ac852c8
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-26 13:46:35 +0100

    Update run_sigprofilerextractor.py

--- 4_mutational_signatures/run_sigprofilerextractor.py
+++ 4_mutational_signatures/run_sigprofilerextractor.py
@@ -2,6 +2,4 @@
 
 from SigProfilerExtractor import sigpro as sig
 
-
-if __name__ == '__main__':
-	sig.sigProfilerExtractor("matrix", "results", "/Users/fernandatalavera/Desktop/BSc_thesis/results/4_mutational_signatures/SigProfiler/output/SBS/vaping.SBS96.exome", reference_genome="mm10", minimum_signatures=1, maximum_signatures=5, nmf_replicates=100, cpu=-1)
+sig.sigProfilerExtractor("matrix", "results", "/Users/fernandatalavera/Desktop/BSc_thesis/results/4_mutational_signatures/SigProfiler/output/SBS/vaping.SBS96.exome", reference_genome="mm10", minimum_signatures=1, maximum_signatures=5, nmf_replicates=100, cpu=-1)
