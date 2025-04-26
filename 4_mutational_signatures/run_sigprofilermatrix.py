aa15687c3ba722b983ae5347dc2727dc329a7316
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-26 13:35:56 +0100

    Update run_sigprofilermatrix.py

--- 4_mutational_signatures/run_sigprofilermatrix.py
+++ 4_mutational_signatures/run_sigprofilermatrix.py
@@ -2,4 +2,4 @@
 
 from SigProfilerMatrixGenerator.scripts import SigProfilerMatrixGeneratorFunc as matGen
 
-matrices = matGen.SigProfilerMatrixGeneratorFunc("vaping","mm10","/mnt/atgc-d1/drobles/ftalavera/vape_lung_cancer/4_mutational_signatures",exome=True, bed_file=None, chrom_based=False, plot=True, tsb_stat=False, seqInfo=True, cushion=100)
+matrices = matGen.SigProfilerMatrixGeneratorFunc("vaping","mm10","/Users/fernandatalavera/Desktop/BSc_thesis/results/4_mutational_signatures/SigProfiler",exome=True, bed_file=None, chrom_based=False, plot=True, tsb_stat=False, seqInfo=True, cushion=100)
