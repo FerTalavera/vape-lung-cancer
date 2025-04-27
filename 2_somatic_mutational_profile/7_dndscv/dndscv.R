1ebb6570375f39476293712651cb96d03c849cd9
FerTalavera <88801225+FerTalavera@users.noreply.github.com>
2025-04-27 11:56:08 +0100

    Update dndscv.R

--- 2_somatic_mutational_profile/7_dndscv/dndscv.R
+++ 2_somatic_mutational_profile/7_dndscv/dndscv.R
@@ -5,6 +5,13 @@ mutations <- read.table("/Users/fernandatalavera/Desktop/BSc_thesis/results/2_so
 names(mutations) <- c("sampleID", "chr", "pos", "ref", "mut")
 mutations <- mutations[, -6]
 
+# removing dinucleotide substitutions
+mutations = mutations[order(mutations$sampleID,mutations$chr,mutations$pos),]
+ind = which(diff(mutations$pos)==1)
+mutations[unique(sort(c(ind,ind+1))),]
+
+mutations_corrected <- mutations[-c(140, 141, 147, 148), ]
+
 load("/Users/fernandatalavera/Desktop/BSc_thesis/results/2_somatic_mutational_profile/dndscv/RefCDS_mouse_GRCm38.p2.rda")
 
 dndsout = dndscv(mutations, refdb = "/Users/fernandatalavera/Desktop/BSc_thesis/results/2_somatic_mutational_profile/dndscv/RefCDS_mouse_GRCm38.p2.rda")
